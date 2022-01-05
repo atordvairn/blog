1445177903d977003db2bfd10260c6c8ba405f32
volcareso <volcareso@gmail.com>
2022-01-05 16:34:24 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+    <ThemeProvider style={{ fontFamily: "'Abel', sans-serif !important" }} attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
