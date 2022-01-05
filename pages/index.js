231ca38e1298357e5745d910de8cca76ae4e9452
volcareso <volcareso@gmail.com>
2022-01-05 16:05:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ posts }) {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1 style={{ color: "black", "background: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs4WbJJttj4nMVNUy6-uQe86aX1qZxQ507hA&usqp=CAU') no-repeat, -webkit-background-clip: "text", -webkit-text-fill-color: "transparent" }} className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
