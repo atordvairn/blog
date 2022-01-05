f99d157a2edea751cda177c202b0a27a29d13903
volcareso <volcareso@gmail.com>
2022-01-05 16:32:59 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,7 @@ class MyDocument extends Document {
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Abel&display=swap"
             rel="stylesheet"
           />
           <link
