8b1f40c3a8461ff39992ad66d9ecd320745916d9
atordvairn <97218417+atordvairn@users.noreply.github.com>
2022-01-10 10:04:28 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -43,7 +43,7 @@ class MyDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-F6FDH8BKXK', {
+            gtag('config', 'G-V33NZW5V69', {
               page_path: window.location.pathname,
             });
           `,
