77dbc26a78774f592a4bf9aa578d66228090f8a8
atordvairn <atordvairn@gmail.com>
2022-01-11 21:06:19 +0530

    Update Link.js

--- components/Link.js
+++ components/Link.js
@@ -4,6 +4,15 @@ import Link from 'next/link'
 const CustomLink = ({ href, ...rest }) => {
   const isInternalLink = href && href.startsWith('/')
   const isAnchorLink = href && href.startsWith('#')
+  const isTagLink = href && href.startsWith('/tags/')
+
+  if (isTagLink) {
+    return (
+      <Link href={href.split(/\/tags\//)[1]}>
+        <a {...rest} />
+      </Link>
+    )
+  }
 
   if (isInternalLink) {
     return (
