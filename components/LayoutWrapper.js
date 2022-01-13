8e8d1a04642b1d8229d748fc16c2730bafd9a844
atordvairn <atordvairn@gmail.com>
2022-01-13 09:40:12 +0530

    Update LayoutWrapper.js

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -18,11 +18,12 @@ const LayoutWrapper = ({ children }) => {
               <div className="flex items-center justify-between">
                 <div className="mr-3">
                   {/* <Logo /> */}
-                  <img
+               {/*<img
                     src="https://i.ibb.co/D9GZb1r/20220106-134100-0000.png"
                     style={{ height: '50px', width: '50px', borderRadius: '15px' }}
                     alt="site's icon"
-                  />
+                  />*/}
+                  <Image src="/static/images/logo.png" height={50} style={{ borderRadius: '15px' }} />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
