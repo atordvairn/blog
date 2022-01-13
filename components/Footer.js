f391975df409f2c26bca7daf74e434632bf26662
atordvairn <atordvairn@gmail.com>
2022-01-13 09:09:30 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,10 +6,10 @@ export default function Footer() {
     <footer>
       <div className="flex flex-col items-center mt-16">
         <div className="flex mb-3 space-x-4">
-          <i class="fas fa-envelope" href={`mailto:${siteMetadata.email}`}></i>
-          <i class="fab fa-github" href={siteMetadata.github}></i>
-          <i class="fab fa-linkedin" href={siteMetadata.linkedin}></i>
-          <i class="fab fa-twitter" href={siteMetadata.twitter}></i>
+          <i className="fas fa-envelope" href={`mailto:${siteMetadata.email}`}></i>
+          <i className="fab fa-github" href={siteMetadata.github}></i>
+          <i className="fab fa-linkedin" href={siteMetadata.linkedin}></i>
+          <i className="fab fa-twitter" href={siteMetadata.twitter}></i>
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
